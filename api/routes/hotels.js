272eb925507de8351b7ff337651f2fd134cd2282
itsme1502 <harshitshankrit@gmail.com>
2023-07-03 17:27:36 +0530

    User Authentication

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,5 +1,4 @@
 import express from "express";
-import Hotel from "../models/Hotel.js";
 import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
 const router = express.Router();
 
