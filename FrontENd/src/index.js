a0caf1e484030f75309738cbf9577521c74bc510
itsme1502 <harshitshankrit@gmail.com>
2023-07-10 01:22:53 +0530

    Fixing Deployment bugs

--- FrontENd/src/index.js
+++ FrontENd/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
 import { SearchContextProvider } from "./context/SearchContext";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://booking-app-ig6s.onrender.com/api"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
