274a6f75256c7e080190055907b527aada406836
itsme1502 <harshitshankrit@gmail.com>
2023-07-10 01:30:12 +0530

    Admin Panel Deployment

--- admin/src/index.js
+++ admin/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { DarkModeContextProvider } from './context/darkModeContext';
 import { AuthContextProvider } from './context/AuthContext';
+import axios from "axios";
 
+axios.defaults.baseURL = "https://booking-app-ig6s.onrender.com/api"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
